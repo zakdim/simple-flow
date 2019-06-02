e3e1b77cc46ea27a4a625e9e747bcaa711b216f9
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-06-02 17:14:46 -0400

    9.1.2 - configuring integration flows in java

--- src/main/java/sia5/FileWriterIntegrationConfig.java
+++ src/main/java/sia5/FileWriterIntegrationConfig.java
@@ -1,8 +1,18 @@
 package sia5;
 
+import java.io.File;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.transformer.GenericTransformer;
+import org.springframework.messaging.MessageChannel;
 
 @Configuration
 public class FileWriterIntegrationConfig {
@@ -11,6 +21,37 @@ public class FileWriterIntegrationConfig {
 	@Configuration
 	@ImportResource("classpath:/filewriter-config.xml")
 	public static class XmlConfiguration {}
+	
+	@Profile("javaconfig")
+	@Bean
+	@Transformer(inputChannel = "textInChannel",
+				 outputChannel = "fileWriterChannel")
+	public GenericTransformer<String, String> upperCaseTransformer() {
+		return text -> text.toUpperCase();
+	}
+	
+	@Profile("javaconfig")
+	@Bean
+	@ServiceActivator(inputChannel = "fileWriterChannel")
+	public FileWritingMessageHandler fileWriter() {
+		FileWritingMessageHandler handler = 
+				new FileWritingMessageHandler(new File("/tmp/sia5/files"));
+		handler.setExpectReply(false);
+		handler.setFileExistsMode(FileExistsMode.APPEND);
+		handler.setAppendNewLine(true);
+		
+		return handler;
+	}
+	
+//	@Bean
+//	public MessageChannel textInChannel() {
+//		return new DirectChannel();
+//	}
+//	
+//	@Bean
+//	public MessageChannel fileWriterChannel() {
+//		return new DirectChannel();
+//	}
 	
 	
 }
