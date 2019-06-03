7c701aa815e9b37318fc2ceb2755387999209c97
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-06-02 21:05:25 -0400

    9.1.3 - using spring integration dsl config

--- src/main/java/sia5/FileWriterIntegrationConfig.java
+++ src/main/java/sia5/FileWriterIntegrationConfig.java
@@ -9,7 +9,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
 import org.springframework.integration.transformer.GenericTransformer;
 import org.springframework.messaging.MessageChannel;
@@ -53,5 +57,20 @@ public class FileWriterIntegrationConfig {
 //		return new DirectChannel();
 //	}
 	
-	
+	//
+	// DSL Configuration
+	//
+	@Profile("javadsl")
+	@Bean
+	public IntegrationFlow fileWriterFlow() {
+		return IntegrationFlows
+				.from(MessageChannels.direct("textInChannel"))
+				.<String, String>transform(t -> t.toUpperCase())
+//				.channel(MessageChannels.direct("fileWriterChannel"))
+				.handle(Files
+						.outboundAdapter(new File("/tmp/sia5/files"))
+						.fileExistsMode(FileExistsMode.APPEND)
+						.appendNewLine(true))
+				.get();
+	}
 }
--- src/main/java/sia5/SimpleFlowApplication.java
+++ src/main/java/sia5/SimpleFlowApplication.java
@@ -1,13 +1,40 @@
 package sia5;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class SimpleFlowApplication {
+	
+	public static final String FILE_NAME = "simple.txt";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleFlowApplication.class, args);
+	}
+
+	// run from command line with profile:
+	// mvn spring-boot:run --Dspring-boot.run.profiles=xmlconfig
+	@Bean
+	public CommandLineRunner writeData(FileWriterGateway gateway, Environment env) {
+		return args -> {
+			String[] activeProfiles = env.getActiveProfiles();
+			if (activeProfiles.length > 0) {
+				String profile = activeProfiles[0];
+				String msg = "Hello, Spring Integration! (" + profile + ")";
+				log.info(String.format("writing message '%s' to file %s via spring integration flow...",
+						msg, FILE_NAME));
+				gateway.writeToFile(FILE_NAME, msg);
+			} else {
+				log.info(
+						"No active profile set. Should set active profile to one of xmlconfig, javaconfig, or javadsl.");
+			}
+		};
 	}
 
 }
